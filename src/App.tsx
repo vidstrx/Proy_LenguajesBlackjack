5d4252f298f24597ff0ee32015150ddef957a768
David Castro <dscastrogarcia@gmail.com>
2026-09-13 16:52:38 -0600

    implementado la creacion y shuffle de cartas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 //import { useState } from 'react'
 //import './App.css'
 
+import Mazo from "./core/Mazo"
+
 function App() {
   //const [count, setCount] = useState(0)
-
+  const mazo = new Mazo();
+  console.log(mazo.getMazo());
   return (
     <>
       <h1>Blackjack</h1>
