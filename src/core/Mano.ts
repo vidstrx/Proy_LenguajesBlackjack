1cd7aa68b6fbe98c063f9ed90512826514de00fc
Diego Madrid <dismadrid1303@gmail.com>
2026-09-14 20:08:37 -0600

    Mano con logica de puntaje y gestion de ases arreglada

--- src/core/Mano.ts
+++ src/core/Mano.ts
@@ -3,23 +3,20 @@ import Carta from './Carta';
 export class Mano {
   public cartas: Carta[] = [];
 
-  // Agregar una carta robada a la mano
   public agregarCarta(carta: Carta): void {
     this.cartas.push(carta);
   }
 
-  // Vaciar la mano para volver a jugar
   public limpiar(): void {
     this.cartas = [];
   }
 
-  // Calcular la puntuación respetando las reglas de Blackjack (As = 11 o 1)
   public calcularPuntaje(): number {
     let total = 0;
     let ases = 0;
 
     for (const carta of this.cartas) {
-      const v = carta.valor; // Lee directamente 'valor' de tu clase Carta
+      const v = carta.valor;
 
       if (v === 'A') {
         ases += 1;
@@ -31,7 +28,7 @@ export class Mano {
       }
     }
 
-    // Si nos pasamos de 21, reducimos el valor de los Ases de 11 a 1
+    // si se pasa de 21, reducimos el valor de los Ases de 11 a 1
     while (total > 21 && ases > 0) {
       total -= 10;
       ases -= 1;
@@ -40,7 +37,6 @@ export class Mano {
     return total;
   }
 
-  // Saber si se pasó de 21
   public estaSePaso(): boolean {
     return this.calcularPuntaje() > 21;
   }
